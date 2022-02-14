b6bf6e7056296f492aaddecd80272d8c23eaa2d1
lxying <18800168665@163.com>
2022-02-14 17:01:04 +0800

    Test Prettier2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-02-14 16:20:12
- * @LastEditTime: 2022-02-14 16:53:22
+ * @LastEditTime: 2022-02-14 17:00:43
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /vue3/Users/xyingliu/code/learn/系统学习/code-prettier/src/index.js
@@ -10,51 +10,41 @@ function HelloWorld({
   greeting = "hello",
   greeted = '"World"',
   silent = false,
-  onMouseOver,} ) {
-  if (!greeting) {return null;
+  onMouseOver,
+}) {
+  if (!greeting) {
+    return null;
   }
 
   // TODO: Don't use random in render
-  let num = Math.floor(Math.random() * 1e7 )
+  let num = Math.floor(Math.random() * 1e7)
     .toString()
     .replace(/\.\d+/gi, "");
 
-  return ( <
-    div className = "HelloWorld"
-    title = {`You are visitor number ${num}`
-    }
-    onMouseOver = {
-      onMouseOver
-    } >
-    <
-    strong > { " "
-    } {
-      greeting.slice(0, 1).toUpperCase() + greeting.slice(1).toLowerCase()
-    } {
-      " "
-    } <
-    /strong>{" "} {  greeting.endsWith(",") ? (
+  return (
+    <div
+      className="HelloWorld"
+      title={`You are visitor number ${num}`}
+      onMouseOver={onMouseOver}
+    >
+      <strong>
+        {" "}
+        {greeting.slice(0, 1).toUpperCase() +
+          greeting.slice(1).toLowerCase()}{" "}
+      </strong>{" "}
+      {greeting.endsWith(",") ? (
         " "
-      ) : ( <
-        span style = {
-          {
+      ) : (
+        <span
+          style={{
             color: "grey",
-          }
-        } >
-        {
-          " "
-        }
-        ", " {
-          " "
-        } <
-        /span>
-      )
-    } {
-      " "
-    } <
-    em > {
-      greeted
-    } < /em> {silent ? "." : "!"}{" "} <
-    /div>
+          }}
+        >
+          {" "}
+          ", "{" "}
+        </span>
+      )}{" "}
+      <em> {greeted} </em> {silent ? "." : "!"}{" "}
+    </div>
   );
 }
